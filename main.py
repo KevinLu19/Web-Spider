f5e54118ddad38c6246423316d587be1e6e9f01b
Kevin <Kevinxz01@gmail.com>
2020-10-31 20:51:51 -0400

    Now properly parses correct url

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ import time
 
 custom_url = "https://maplestory.nexon.net/News/"
 
-try:
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.set_headless()
-    brower = webdriver.Firefox(firefox_options=fireFoxOptions)
+def grab_URL(query_data):
+    new_string = query_data.split('href="')[1].split('"')[0]
 
-    brower.get(custom_url)
+    return new_string
 
-    news_container = []
+if __name__ == "__main__":
+    try:
+        fireFoxOptions = webdriver.FirefoxOptions()
+        fireFoxOptions.set_headless()
+        brower = webdriver.Firefox(firefox_options=fireFoxOptions)
 
-    maple_news = brower.find_element_by_class_name("news-wrapper")
-    news_ul = maple_news.find_elements_by_tag_name("ul")
+        brower.get(custom_url)
+        maple_news = brower.find_element_by_class_name("news-wrapper")
+        news_ul = maple_news.find_elements_by_tag_name("ul")
 
-    for query_item in news_ul:
+        for query_item in news_ul:
         # text_form = query_item.text
         # print (text_form)
 
-        query_innerHTML = query_item.get_attribute("innerHTML")
-        query_split_lines = query_innerHTML.strip().splitlines()
+            query_innerHTML = query_item.get_attribute("innerHTML")
+            query_split_lines = query_innerHTML.strip().split()
 
-        for items in query_split_lines:
-            if "href=" in items:
-                print (items)
+            #print (query_split_lines)
 
+            for items in query_split_lines:
+                if "href=" in items:
+                    #query_href = items.split()
+                    # query_container.append(items)
 
+                    print(grab_URL(items))
 
-finally:
-    try:
-        brower.close()
-    except:
-        pass
+    finally:
+        try:
+            brower.close()
+        except:
+            pass
+
+    query_container = []
