16cb19c027e816d2f60b295a14b71a30121e9a12
Kevin <Kevinxz01@gmail.com>
2020-11-02 09:39:40 -0500

    Added checking url via Validators module

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 
 import time
+import validators
 
-custom_url = "https://maplestory.nexon.net/News/"
+# custom_url = "https://maplestory.nexon.net/News/"
 
 def grab_URL(query_data):
     new_string = query_data.split('href="')[1].split('"')[0]
 
     return new_string
+
+def validify_URL(checking_url):
+    valid_url = validators.url(checking_url)
+
+    if valid_url == True:
+        return checking_url
 
 if __name__ == "__main__":
     try:
@@ -15,30 +22,26 @@ if __name__ == "__main__":
         fireFoxOptions.set_headless()
         brower = webdriver.Firefox(firefox_options=fireFoxOptions)
 
-        brower.get(custom_url)
+        user_input_url = input ("Enter URL: ")
+        working_url = validify_URL(user_input_url)
+
+        brower.get(str(working_url))
         maple_news = brower.find_element_by_class_name("news-wrapper")
         news_ul = maple_news.find_elements_by_tag_name("ul")
 
-        for query_item in news_ul:
-        # text_form = query_item.text
-        # print (text_form)
+        query_container = []
 
+        for query_item in news_ul:
             query_innerHTML = query_item.get_attribute("innerHTML")
             query_split_lines = query_innerHTML.strip().split()
 
-            #print (query_split_lines)
-
             for items in query_split_lines:
                 if "href=" in items:
-                    #query_href = items.split()
-                    # query_container.append(items)
-
-                    print(grab_URL(items))
+                    each_items = grab_URL(items)
+                    query_container.append(each_items)
 
     finally:
         try:
             brower.close()
         except:
             pass
-
-    query_container = []
